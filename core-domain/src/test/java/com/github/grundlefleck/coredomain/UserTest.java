02194522667c10cbfc9abf1abe9ea66925a703b9
Graham Allan <Grundlefleck@gmail.com>
2020-03-21 17:55:50 +0000

    See a build go from failed to succeeded due to a test.

--- core-domain/src/test/java/com/github/grundlefleck/coredomain/UserTest.java
+++ core-domain/src/test/java/com/github/grundlefleck/coredomain/UserTest.java
@@ -2,11 +2,9 @@ package com.github.grundlefleck.coredomain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 class UserTest {
 
     @Test public void someTest() {
-        fail();
+
     }
 }
